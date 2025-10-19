9f453dad83f6f874f4d6b21e74f1cb2b18947605
кирюха <kirillgarai8@mgail.com>
2025-10-20 00:52:48 +0300

    Changed to plus icon with frame border

--- script.js
+++ script.js
@@ -1582,8 +1582,9 @@ function updateActiveNavButton(activeButton) {
             const icon = button.querySelector('.nav-icon');
             if (icon) {
                 icon.innerHTML = `
-                    <line x1="12" y1="5" x2="12" y2="19"></line>
-                    <line x1="5" y1="12" x2="19" y2="12"></line>
+                    <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
+                    <line x1="12" y1="8" x2="12" y2="16"></line>
+                    <line x1="8" y1="12" x2="16" y2="12"></line>
                 `;
             }
         }
