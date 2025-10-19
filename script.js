8466f0a71c51951b1e85c4e2529b03cb5a12a651
кирюха <kirillgarai8@mgail.com>
2025-10-20 00:54:06 +0300

    Made plus icon larger inside the frame

--- script.js
+++ script.js
@@ -1583,8 +1583,8 @@ function updateActiveNavButton(activeButton) {
             if (icon) {
                 icon.innerHTML = `
                     <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
-                    <line x1="12" y1="8" x2="12" y2="16"></line>
-                    <line x1="8" y1="12" x2="16" y2="12"></line>
+                    <line x1="12" y1="6" x2="12" y2="18"></line>
+                    <line x1="6" y1="12" x2="18" y2="12"></line>
                 `;
             }
         }
